6ac7650c2f582c3c9b635bd57339de8ba16b814c
Vasyl <muran.wasyl@ukr.net>
2020-04-20 21:31:51 +0300

    Updates

---
+++ precache-manifest.7e0edcf84b3fa8f2065e4842a8dbbadc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05d8ce01b125612e6ea1a5a8d25ba48b",
+    "url": "/timer/index.html"
+  },
+  {
+    "revision": "5cd86488299207d753de",
+    "url": "/timer/static/css/main.87bd063b.chunk.css"
+  },
+  {
+    "revision": "89562e421e23eac50ac8",
+    "url": "/timer/static/js/2.eb2f27dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/timer/static/js/2.eb2f27dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cd86488299207d753de",
+    "url": "/timer/static/js/main.a59251f6.chunk.js"
+  },
+  {
+    "revision": "c5e3574b68ae10966c2f",
+    "url": "/timer/static/js/runtime-main.7ed3bfb2.js"
+  }
+]);
